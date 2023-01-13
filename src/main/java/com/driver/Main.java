78d04731d7f6fa9d63c115caff9e2c6133890806
Parth Agrawal <aparth443@gmail.com>
2023-01-13 14:08:37 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //java: name has private access in com.driver.RWOnly
+        rw.setName("aparth443");
+        String ans = rw.getName();
+        System.out.println(ans);
+    }
 }
